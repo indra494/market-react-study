16bba37cc601205a2800e8d4fcf109ec221efe76
indra494 <indra494@gmail.com>
2023-03-09 01:47:35 +0900

    buy product

--- src/product/index.js
+++ src/product/index.js
@@ -4,12 +4,13 @@ import { useEffect, useState } from "react";
 import './index.css';
 import { API_URL } from "../config/constatns.js"
 import dayjs from "dayjs";
+import { Button, message } from "antd";
 
 function ProductPage() {
     const {id} = useParams();
     const [product, setProduct] = useState(null);
 
-    useEffect(function() {
+    const getProduct = () => {
         axios.get(`${API_URL}/products/${id}`)
             .then(function(result) {
                 setProduct(result.data.product);
@@ -17,10 +18,24 @@ function ProductPage() {
             .catch(function(error) {
                 console.error(error);
             });
+    }
+
+    useEffect(function() {
+        getProduct();
     },[]);
 
     if(product === null) {
         return <h1>상품 정보를 받고 있습니다...</h1>
+    }
+
+    const onClickPurchase = () => {
+        axios.post(`${API_URL}/purchase/${id}`)
+        .then((result) => {
+            message.info('구매가 완료되었습니다.');
+            getProduct();
+        }).catch((error)=> {
+            message.error(`에러가 발생하였습니다. ${error.message}`);
+        });
     }
 
     return (
@@ -35,7 +50,17 @@ function ProductPage() {
             <div id="contents-box">
                 <div id="name">{product.name}</div>
                 <div id="price">{product.price}원</div>
-                <div id="createdAt">{dayjs(product.createdAt).format('YYYY년 MM월 DD일')}</div>                
+                <div id="createdAt">{dayjs(product.createdAt).format('YYYY년 MM월 DD일')}</div>        
+                <Button 
+                    id="purchase-button" 
+                    size="large" 
+                    type="primary" 
+                    danger 
+                    onClick={onClickPurchase}
+                    disabled={product.soldout === 1}
+                >
+                    재빨리 구매하기
+                </Button>        
                 <pre id="description">{product.description}</pre>
             </div>
         </div>
